1b70a2fa4157d4714b1b08ccf38d125003c94164
Grace Daniel <28490999+GraciaDe@users.noreply.github.com>
2022-03-28 16:20:48 +0100

    Main file
    Currently puts all fish info into csv. Not the neatest and definitely needs cleaning.

---
+++ fish.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import csv
+
+
+r = requests.get("https://stardewvalleywiki.com/Fish")
+fish = bs(r.content, "html.parser")
+
+# Headers for each categorey
+a = fish.select("table:nth-of-type(1) th")
+title = [z.text.strip() for z in a]
+
+
+Headers = [title[1], title[6], title[7], title[8], title[9]]
+
+# Name of each fish
+b = fish.select("table:nth-of-type(1) tr td:nth-of-type(2) a[title]:nth-of-type(1)")
+name = [y.text.strip() for y in b]
+
+# Locations
+c = fish.select("table:nth-of-type(1) tr td:nth-of-type(7)")
+location = [x.text.strip() for x in c]
+
+# Times
+d = fish.select("table:nth-of-type(1) tr td:nth-of-type(8)")
+time = [w.text.strip().replace(u"\xa0", "") for w in d]
+
+# Seasons
+e = fish.select("table:nth-of-type(1) tr td:nth-of-type(9)")
+season = [v.text.strip().replace(u"\xa0", ", ") for v in e]
+
+# Weather
+f = fish.select("table:nth-of-type(1) tr td:nth-of-type(10)")
+weather = [u.text.strip().replace(u"\xa0", ", ") for u in f]
+
+
+with open("D:\grace\Documents\StardewFish.csv", "w") as file:
+    write = csv.writer(file)
+
+    data = list(zip(name, location, time, season, weather))
+    data.insert(0, Headers)
+
+    for row in data:
+        row = list(row)
+        write.writerow(row)
+
